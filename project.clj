ebf1bb651428855eab795b328f53b0b262a2a07e
Philipp Küng <git@philippkueng.ch>
2015-08-16 18:22:24 +0200

    adding a description and url to the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bitfondue-website "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The bitfondue website repository"
+  :url "https://bitfondue.com"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
