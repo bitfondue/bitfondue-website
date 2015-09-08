3793d1291bd474e5c62b5b4da46e8bdba21361ff
Philipp Küng <git@philippkueng.ch>
2015-09-08 18:04:36 +0200

    defining dependencies to get compojure and jetty up and running

--- project.clj
+++ project.clj
@@ -4,8 +4,12 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [environ "1.0.0"]]
+                 [environ "1.0.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [ring/ring-core "1.3.1"]]
   :plugins [[lein-environ "1.0.0"]
+            [lein-ring "0.8.11"]
             [codox "0.8.13"]]
   :codox {:src-dir-uri "https://github.com/bitfondue/bitfondue-website/blob/master/"
           :src-linenum-anchor-prefix "L"})
