1e31552fda75d9b0c49d2ce7ae79b53bbd1666e5
Philipp Küng <git@philippkueng.ch>
2015-09-09 17:35:11 +0200

    returning a 200 status code for the root url, making the first fact pass

--- src/bitfondue/handler.clj
+++ src/bitfondue/handler.clj
@@ -1,4 +1,11 @@
-(ns bitfondue.handler)
+(ns bitfondue.handler
+  (:require (compojure [core :refer :all]
+                       [route :as route])))
+
+(defroutes app-routes
+  (GET "/" [] "Success!"))
+
+(def app app-routes)
 
 (defn foo
   "I don't do a whole lot."
