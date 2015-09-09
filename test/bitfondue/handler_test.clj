3a97b4cb9e2a5f575e1a272d1d18ca729bb30201
Philipp Küng <git@philippkueng.ch>
2015-09-09 17:23:18 +0200

    adding a first fact making sure that the root route returns a status code of 200

--- test/bitfondue/handler_test.clj
+++ test/bitfondue/handler_test.clj
@@ -3,3 +3,7 @@
             [ring.mock.request :as mock])
   (:use midje.sweet))
 
+(fact "The root route / is returning a 200 status code"
+  (->> (mock/request :get "/")
+       app
+       :status) => 200)
