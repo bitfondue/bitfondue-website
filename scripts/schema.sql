7d7660eba043f88c033d5a6fdde8aef8a069c309
Philipp Küng <git@philippkueng.ch>
2015-09-13 17:14:09 +0200

    added the database schema containing only the users table so far

---
+++ scripts/schema.sql
@@ -0,0 +1,16 @@
+--
+-- Table structure for table "users"
+--
+
+-- DROP TABLE IF EXISTS "users";
+CREATE TABLE "users" (
+  "username" varchar(255) NOT NULL,
+  "email" varchar(255) NOT NULL,
+  "password" varchar(100) NOT NULL,
+  "lastname" varchar(255) DEFAULT NULL,
+  "firstname" varchar(255) DEFAULT NULL,
+  "updated_on" date DEFAULT NULL,
+  "created_on" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "id" SERIAL,
+  PRIMARY KEY ("id")
+);
