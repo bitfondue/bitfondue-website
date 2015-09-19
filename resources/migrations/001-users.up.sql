5e47e8bd6c14b873d70dcd4aa63bee693450d310
Philipp Küng <git@philippkueng.ch>
2015-09-19 15:08:27 +0200

    adding sql migration files for the users table

---
+++ resources/migrations/001-users.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+  "username" varchar(255) NOT NULL,
+  "email" varchar(255) NOT NULL,
+  "password" varchar(100) NOT NULL,
+  "lastname" varchar(255) DEFAULT NULL,
+  "firstname" varchar(255) DEFAULT NULL,
+  "updated_on" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "created_on" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "id" SERIAL,
+  PRIMARY KEY ("id")
+);
