f52da256bac45f0d5f44f2ac6a2afbdc00e3edc0
Philipp Küng <git@philippkueng.ch>
2015-09-20 16:38:55 +0200

    starting with parsing the existing database dump into clojure data structures

--- src/bitfondue/migrations.clj
+++ src/bitfondue/migrations.clj
@@ -1,7 +1,8 @@
 (ns bitfondue.migrations
   (:require (ragtime [jdbc :as jdbc]
                      [repl :as repl])
-            [bitfondue.config :as config]))
+            [bitfondue.config :as config]
+            [cheshire.core :refer :all]))
 
 (defn load-config []
   {:datastore  (jdbc/sql-database config/database)
@@ -13,3 +14,10 @@
 (defn rollback []
   (repl/rollback (load-config)))
 
+
+;; migrate the existing data into the new database
+;; parse the json line entries into a collection of maps
+(def ed (with-open [rdr (clojure.java.io/reader "existing_data.json")]
+          (doall (->> rdr
+                      line-seq
+                      (map #(parse-string % true))))))
