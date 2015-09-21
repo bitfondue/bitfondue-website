1bc8f20eedd38a223ca474bad9fbfcc1c3a8fca4
Philipp Küng <git@philippkueng.ch>
2015-09-21 18:42:16 +0200

    adding friend and wrap the routes in friend/authenticate, providing validation functions and redirect defaults

--- src/bitfondue/handler.clj
+++ src/bitfondue/handler.clj
@@ -1,13 +1,27 @@
 (ns bitfondue.handler
   (:require (compojure [core :refer :all]
-                       [route :as route]))
+                       [route :as route])
+            [cemerick.friend :as friend]
+            (cemerick.friend [workflows :as workflows]
+                             [credentials :as creds])
+            [ring.util.response :as resp]
+            (bitfondue.models [users :as users]))
   (:use [ring.adapter.jetty :as ring]))
 
 (defroutes app-routes
   (GET "/" [] "Success!")
   (route/resources "/"))
 
-(def app app-routes)
+(def app (friend/authenticate
+          app-routes
+          {:allow-anon? true
+           :login-uri "/login"
+           :default-landing-uri "/"
+           :unauthorized-handler #(-> "Not Authorized"
+                                      resp/response
+                                      (resp/status 401))
+           :credential-fn #(creds/bcrypt-credential-fn users/get-user %)
+           :workflows [(workflows/interactive-form)]}))
 
 (defn -main [port]
   (run-jetty app {:port (read-string port) :join? false}))
