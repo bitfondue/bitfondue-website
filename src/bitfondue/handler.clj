22ea134d9bdf63f60f47929b55dae8ebdb6a3d34
Philipp Küng <git@philippkueng.ch>
2015-09-24 07:49:07 +0200

    using a first copy of the implementation done by @funcool over in the buddy-auth repository
    https://github.com/funcool/buddy-auth/tree/master/examples/token-jwe

--- src/bitfondue/handler.clj
+++ src/bitfondue/handler.clj
@@ -18,11 +18,46 @@
   (:use [ring.adapter.jetty :as ring])
   (:gen-class))
 
+(def secret (nonce/random-bytes 32))
+(defn ok [d] {:status 200 :body d})
+(defn bad-request [d] {:status 400 :body d})
+
+(defn home
+  [request]
+  (if-not (authenticated? request)
+    (throw-unauthorized)
+    (ok {:status "Logged" :message (str "hello logged user "
+                                        (:identity request))})))
+
+(def authdata {:admin "secret"
+               :test "secret"})
+
+(defn login
+  [request]
+  (let [username (get-in request [:body :username])
+        password (get-in request [:body :password])
+        valid? (some-> authdata
+                       (get (keyword username))
+                       (= password))]
+    (if valid?
+      (let [claims {:user (keyword username)
+                    :exp (time/plus (time/now) (time/seconds 3600))}
+            token (jwe/encrypt claims secret {:alg :a256kw :enc :a128gcm})]
+        (ok {:token token}))
+      (bad-request {:message "wrong auth data"}))))
+
 (defroutes app-routes
   (GET "/" [] home)
   (POST "/login" [] login)
   (route/resources "/"))
 
+(def auth-backend (jwe-backend {:secret secret :options {:alg :a256kw :enc :a128gcm}}))
+
+(def app (-> app-routes
+             (wrap-authorization auth-backend)
+             (wrap-authentication auth-backend)
+             (wrap-json-response {:pretty false})
+             (wrap-json-body {:keywords? true :bigdecimals? true})))
 
 (defn -main [port]
   (run-jetty app {:port (read-string port) :join? false}))
