4dc66b54004cb6a9b743f24df9b0319636e547c4
Philipp Küng <git@philippkueng.ch>
2015-09-24 07:45:57 +0200

    adding the newly added dependencies with requires
    requiring parts of buddy, more ring middleware to deal with request parsing and JSON

--- src/bitfondue/handler.clj
+++ src/bitfondue/handler.clj
@@ -1,12 +1,22 @@
 (ns bitfondue.handler
   (:require (compojure [core :refer :all]
-                       [route :as route])
-            [cemerick.friend :as friend]
-            (cemerick.friend [workflows :as workflows]
-                             [credentials :as creds])
-            [ring.util.response :as resp]
+                       [route :as route]
+                       [response :refer [render]])
+            [clojure.java.io :as io]
+            [ring.util.response :refer [response redirect content-type status]]
+            (ring.middleware [session :refer [wrap-session]]
+                             [params :refer [wrap-params]]
+                             [json :refer [wrap-json-response wrap-json-body]])
+            [hiccup.core :as h]
+            [clj-time.core :as time]
+            [buddy.sign.jwe :as jwe]
+            [buddy.core.nonce :as nonce]
+            [buddy.auth :refer [authenticated? throw-unauthorized]]
+            [buddy.auth.backends.token :refer [jwe-backend]]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
             (bitfondue.models [users :as users]))
-  (:use [ring.adapter.jetty :as ring]))
+  (:use [ring.adapter.jetty :as ring])
+  (:gen-class))
 
 (defroutes app-routes
   (GET "/" [] "Success!")
