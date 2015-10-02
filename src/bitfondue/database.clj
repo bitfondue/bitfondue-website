1f7b292992c1039bd931d31fedf2117f78b23e81
Philipp Küng <git@philippkueng.ch>
2015-10-02 21:14:13 +0200

    extending the pgobject as mentioned in the yesql issue 54
    https://github.com/krisajenkins/yesql/issues/54

--- src/bitfondue/database.clj
+++ src/bitfondue/database.clj
@@ -1,5 +1,17 @@
 (ns bitfondue.database
-  (:require [yesql.core :refer [defquery]]))
+  (:require [cheshire.core :as cheshire]
+            [yesql.core :refer [defquery]]
+            [clojure.java.jdbc :as jdbc])
+  (:import [org.postgresql.util PGobject]))
+
+(extend-type org.postgresql.util.PGobject
+  jdbc/IResultSetReadColumn
+  (result-set-read-column [val rsmeta idx]
+    (let [colType (.getColumnTypeName rsmeta idx)]
+      (if (or (= colType "json")
+              (= colType "jsonb"))
+        (cheshire/parse-string (.getValue val) true)
+        val))))
 
 (defquery insert-user<! "bitfondue/sql/insert-user.sql")
 (defquery get-user "bitfondue/sql/get-user.sql")
