3bea197d94fc6651b9a57a2ff01d18ba1590cbda
Philipp Küng <git@philippkueng.ch>
2015-10-03 12:32:59 +0200

    adding comments to the sql queries

--- src/bitfondue/queries.sql
+++ src/bitfondue/queries.sql
@@ -1,10 +1,12 @@
 -- name: get-user
+-- retrieve a user given a username
 SELECT username,email,firstname,lastname,password
 FROM users
 WHERE username = :username
 LIMIT 1
 
 -- name: insert-user<!
+-- insert a new user
 INSERT INTO users
 (username,email,password)
 VALUES (:username,:email,:password);
