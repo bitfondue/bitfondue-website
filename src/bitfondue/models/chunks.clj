739ba30f5d069cccc4305242592333c287bb4a9e
Philipp Küng <git@philippkueng.ch>
2015-10-13 07:34:40 +0200

    added the insert-chunk! function to the models.chunk namespace

--- src/bitfondue/models/chunks.clj
+++ src/bitfondue/models/chunks.clj
@@ -1,1 +1,10 @@
-(ns bitfondue.models.chunks)
+(ns bitfondue.models.chunks
+  (:require (bitfondue [config :as config]
+                       [database :as database])))
+
+(defn insert-chunk!
+  "Insert a chunk into the database"
+  [chunk]
+  (database/insert-chunk<! config/database
+                           (:title chunk)))
+
