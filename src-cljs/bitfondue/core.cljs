825b64c640b321f50b76da3338890626fe1a50dc
Philipp Küng <git@philippkueng.ch>
2015-10-16 17:28:40 +0200

    add a list of chunks via the /chunks API route

--- src-cljs/bitfondue/core.cljs
+++ src-cljs/bitfondue/core.cljs
@@ -10,17 +10,46 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
+(defonce chunks-state (atom '({:title "first title"}
+                              {:title "second title"})))
 
 (defn hello-world []
   [:h1 (:text @app-state)])
 
-(reagent/render-component [hello-world]
+(defn chunks
+  []
+  [:ul
+   (for [item (->> @chunks-state
+                   (sort-by :created_on))]
+     [:li (:title item)])])
+
+(defn layout
+  []
+  [:div
+   [hello-world]
+   [chunks]])
+
+(reagent/render-component [layout]
                           (. js/document (getElementById "app")))
 
+(defn chunks-success-handler
+  [response]
+  (reset! chunks-state (:chunks response)))
+
+(defn chunks-error-handler
+  [{:keys [status status-text]}]
+  nil)
+
+(defn fetch-chunks
+  []
+  (GET "/chunks" {:response-format :json
+                  :keywords? true
+                  :handler chunks-success-handler
+                  :error-handler chunks-error-handler}))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  (reset! app-state {:text (pr-str (GET "/chunks"))})
+  (fetch-chunks)
 )
