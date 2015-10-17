4d2890608f43d8f364a5714592ea3073dd1e81f0
Philipp Küng <git@philippkueng.ch>
2015-10-17 12:57:26 +0200

    disable the developer console since it's currently not needed

--- src-cljs/bitfondue/core.cljs
+++ src-cljs/bitfondue/core.cljs
@@ -3,10 +3,6 @@
      [reagent.core :as reagent :refer [atom]]
      [ajax.core :refer [GET]]))
 
-(enable-console-print!)
-
-(println "Edits to this text should show up in your developer console.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
