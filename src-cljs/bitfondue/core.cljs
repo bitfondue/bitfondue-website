52f926dfda828e813121e9a44997d69b22834b4a
Philipp Küng <git@philippkueng.ch>
2015-11-06 22:14:47 +0100

    made the header navigation centered

--- src-cljs/bitfondue/core.cljs
+++ src-cljs/bitfondue/core.cljs
@@ -21,19 +21,20 @@
 (defn header
   []
   [:nav.navbar.navbar-light.bg-faded
-   [:a.navbar-brand {:href "/"} "bitfondue"]
-   [:ul.nav.navbar-nav
-    [:li.nav-item
-     [:a.nav-link {:href "#"} "Features"]]
-    [:li.nav-item
-     [:a.nav-link {:href "#"} "Pricing"]]
-    [:li.nav-item
-     [:a.nav-link {:href "#"} "About"]]]
-   [:ul.nav.navbar-nav.pull-right
-    [:li.nav-item
-     [:a.nav-link {:href "#"} "Login"]]
-    [:li.nav-item
-     [:a.nav-link {:href "#"} "Register"]]]])
+   [:div.container
+    [:a.navbar-brand {:href "/"} "bitfondue"]
+    [:ul.nav.navbar-nav
+     [:li.nav-item
+      [:a.nav-link {:href "#"} "Features"]]
+     [:li.nav-item
+      [:a.nav-link {:href "#"} "Pricing"]]
+     [:li.nav-item
+      [:a.nav-link {:href "#"} "About"]]]
+    [:ul.nav.navbar-nav.pull-right
+     [:li.nav-item
+      [:a.nav-link {:href "#"} "Login"]]
+     [:li.nav-item
+      [:a.nav-link {:href "#"} "Register"]]]]])
 
 (defn layout
   []
