0813300e292ba935f7e1f68fa7ec07d7d25548c2
Philipp Küng <git@philippkueng.ch>
2015-11-12 07:23:00 +0100

    adding a url and description_summary field to the chunks table

--- resources/migrations/002.chunks.up.sql
+++ resources/migrations/002.chunks.up.sql
@@ -4,6 +4,8 @@
 
 CREATE TABLE "chunks" (
   "title" text NOT NULL,
+  "url" text NOT NULL,
+  "description_summary" text,
   "created_on" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "id" SERIAL,
   PRIMARY KEY ("id")
