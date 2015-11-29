dca44fb35abedf2aeb6f42ad476caadad2bddbfd
Philipp Küng <git@philippkueng.ch>
2015-11-29 18:05:38 +0000

    remove the description_summary field from the chunks migration file

--- resources/migrations/002.chunks.up.sql
+++ resources/migrations/002.chunks.up.sql
@@ -6,7 +6,6 @@ CREATE TABLE "chunks" (
   "title" text NOT NULL,
   "url" text NOT NULL,
   "description_full" text,
-  "description_summary" text,
   "uid" text NOT NULL,
   "created_on" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "id" SERIAL,
