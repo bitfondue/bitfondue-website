aa005852dd026ec41e868a2427b6a66f30d999af
Philipp Küng <git@philippkueng.ch>
2015-11-30 18:40:03 +0000

    extending the gulpfile by adding a watch task that keeps tabs on the css files and recompiles if necessary

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat');
 
+var paths = {
+  css: 'resources/css/*.css'
+}
+
 gulp.task('styles', function () {
   gulp.src([
     'bower_components/bootstrap/dist/css/bootstrap.min.css',
-    'resources/css/app.css'
+    paths.css
   ])
   .pipe(concat('app.min.css'))
   .pipe(gulp.dest('resources/public/css'));
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', function () {
+  gulp.watch(paths.css, ['styles']);
+});
+
+gulp.task('build', ['styles']);
