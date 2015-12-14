990fcc3b5d81104c04be41247e08a10de2d0e709
Philipp Küng <git@philippkueng.ch>
2015-12-14 07:20:08 +0100

    serialize the :data map when inserting a chunk into the database

--- src/bitfondue/models/chunks.clj
+++ src/bitfondue/models/chunks.clj
@@ -1,11 +1,13 @@
 (ns bitfondue.models.chunks
   (:require (bitfondue [config :as config]
-                       [database :as database])))
+                       [database :as database])
+            [cheshire.core :as cheshire]))
 
 (defn insert-chunk!
   "Insert a chunk into the database"
   [chunk]
-  (database/insert-chunk<! chunk))
+  (database/insert-chunk<! (assoc chunk
+                                  :data (cheshire/generate-string (:data chunk)))))
 
 (defn get-chunks
   "Retrieve chunks"
